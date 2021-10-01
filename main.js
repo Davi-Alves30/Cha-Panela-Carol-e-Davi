028c4806a31db6ae7cb0c514592c82151ed3ea4c
Davi Alves de Jesus <daviapc888@gmail.com>
2021-10-01 16:54:41 -0300

    Adicionando alterações do Samuel

--- main.js
+++
@@ -1,9 +0,0 @@
-const novaTarefa = document.querySelector('.botao-confirmar');
-
-const input = document.querySelector('input');
-
-const valor = input.checked;
-
-console.log(valor);
-
-novaTarefa.addEventListener('click', () => {console.log("Foi clicado")});
